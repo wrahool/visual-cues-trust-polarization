3ca52f52b294d679269d2c9cafb50764eb09910a
Subhayan Mukerjee <mail@subhayan.com>
2025-07-11 10:55:49 +0800

    change pathname in script

--- scripts/06_descriptives.R
+++ scripts/06_descriptives.R
@@ -11,7 +11,7 @@ scale_variable <- function(x) {
   (x - min(x)) / (max(x) - min(x))
 }
 
-dat <- read_csv("auxiliary/preprocessed-final-prolific-for-analysis.csv")
+dat <- read_csv("data/visual-cues_anonymised-data.csv")
 
 glimpse(dat)
 
